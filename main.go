738ffb05477fdc14b7b856b7af51c0e02b982818
Victhor Sartório <victhor@vsartor.com>
2018-12-23 12:52:05 -0200

    Add compilation and running mode for QoL

--- main.go
+++ main.go
@@ -54,13 +54,25 @@ func main() {
 		}
 		disassemble(args[1+debugOffset], logger)
 
+	case "cr":
+		logger.Println("Compilation and running mode has been set.")
+		if len(args) != 3+debugOffset {
+			fmt.Fprint(os.Stderr, "gvm: Expected two files after 'cr'.\n")
+			os.Exit(1)
+		}
+		logger.Println("Starting compilation mode.")
+		compile(args[1+debugOffset], args[2+debugOffset], logger)
+		logger.Println("Starting running mode.")
+		run(args[2+debugOffset], logger)
+
 	case "h":
 		fmt.Println("gvm [d|debug] <run mode> [file] [output]")
 		fmt.Println("Run mode options:")
-		fmt.Println("- h: Shows usage.")
-		fmt.Println("- c: Compiles a file.")
-		fmt.Println("- r: Runs a compiled file.")
-		fmt.Println("- d: Disassemble and pretty print a compiled file.")
+		fmt.Println("- h:  Shows usage.")
+		fmt.Println("- c:  Compiles a file.")
+		fmt.Println("- r:  Runs a compiled file.")
+		fmt.Println("- d:  Disassemble and pretty print a compiled file.")
+		fmt.Println("- cr: Compiles a file and then runs it.")
 
 	default:
 		fmt.Fprintf(os.Stderr,
