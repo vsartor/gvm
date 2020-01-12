5bb1f7caeceebbdb9b950f4ff02aae34e0097ec7
Victhor Sartório <victhor.sartorio@corp.globo.com>
2020-01-12 00:21:11 -0300

    Remove awful debugOffset

--- main.go
+++ main.go
@@ -26,53 +26,58 @@ func main() {
 	var ctxt gvm.Context
 
 	// Check early if we're actually doing logging
-	debugOffset := 0
+	hasLogging := false
 	if args[0] == "l" {
-		debugOffset++
 		gvm.Logger.SetLogLevel(loggo.INFO)
+		hasLogging = true
 	} else if args[0] == "L" {
-		debugOffset++
 		gvm.Logger.SetLogLevel(loggo.DEBUG)
+		hasLogging = true
 	}
 
 	// Error out in case only a debug flag was passed
-	if debugOffset == 1 && len(args) == 1 {
+	if hasLogging && len(args) == 1 {
 		appLogger.Fatalln("Only received a debug flag.")
 	}
 
+	// Remove logging argument from args
+	if hasLogging {
+		args = args[1:]
+	}
+
 	// Dispatch into the correct routine
-	switch runMode := args[0+debugOffset]; runMode {
+	switch runMode := args[0]; runMode {
 	case "c", "compile":
-		if len(args) != 3+debugOffset {
+		if len(args) != 3 {
 			appLogger.Fatalln("Expected two files after 'compile': <source_path>, <object_path>")
 		}
-		compiler.Compile(args[1+debugOffset], args[2+debugOffset], ctxt)
+		compiler.Compile(args[1], args[2], ctxt)
 
 	case "r", "run":
-		if len(args) != 2+debugOffset {
+		if len(args) != 2 {
 			appLogger.Fatalln("Expected one file after 'run': <object_path>")
 		}
-		vm.Run(args[1+debugOffset], ctxt)
+		vm.Run(args[1], ctxt)
 
 	case "d", "disassemble":
-		if len(args) != 2+debugOffset {
+		if len(args) != 2 {
 			appLogger.Fatalln("Expected one file after 'disassemble': <object_path>")
 		}
-		vm.Disassemble(args[1+debugOffset], ctxt)
+		vm.Disassemble(args[1], ctxt)
 
 	case "cr":
-		if len(args) != 3+debugOffset {
+		if len(args) != 3 {
 			appLogger.Fatalln("Expected two files after 'cr': <source_path>, <object_path>")
 		}
-		compiler.Compile(args[1+debugOffset], args[2+debugOffset], ctxt)
-		vm.Run(args[2+debugOffset], ctxt)
+		compiler.Compile(args[1], args[2], ctxt)
+		vm.Run(args[2], ctxt)
 
 	case "cd":
-		if len(args) != 3+debugOffset {
+		if len(args) != 3 {
 			appLogger.Fatalln("Expected two files after 'cd': <source_path>, <object_path>")
 		}
-		compiler.Compile(args[1+debugOffset], args[2+debugOffset], ctxt)
-		vm.Disassemble(args[2+debugOffset], ctxt)
+		compiler.Compile(args[1], args[2], ctxt)
+		vm.Disassemble(args[2], ctxt)
 
 	case "h", "help":
 		fmt.Println("gvm [logging flag] <command> [input file] [output file]")
